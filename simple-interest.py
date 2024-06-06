1403eae6b5cf5ce6654c660ffb3ca88759839609
ramyerubandi1 <rmrbnd@example.com>
2024-06-06 18:00:34 +0000

    ass1-q1

---
+++ simple-interest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+p = input("enter principal amount:")
+r = input("enter interest percentage rate:")
+t = input("enter time:")
+s = (int(p) * (int(r)/100) * int(t))
+print("simple interest is;",s)
+
+
